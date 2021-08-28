6778e4011c62c2b9d8ba022fb399a47921aeae95
David Siegel <djsiegel@gmail.com>
2021-08-28 14:57:56 -0700

    Revert "Small change to test cache busting"
    This reverts commit 5f96e6c930ebda9cd65b686bc60ada2e5036e94e.

--- src/column.test.ts
+++ src/column.test.ts
@@ -2,5 +2,5 @@ import main from "./column";
 
 test("We can get an echo", async () => {
   const response = await main({ type: "string", value: "foo" });
-  expect(response).toBe("echo foo!");
+  expect(response).toBe("echo foo");
 });
