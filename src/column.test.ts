7e837f00d611df44d834cf39573192a3b2ca6633
David Siegel <djsiegel@gmail.com>
2021-08-28 13:07:08 -0700

    add emotion

--- src/column.test.ts
+++ src/column.test.ts
@@ -7,6 +7,16 @@ test("We have an API key", () => {
 test("We can get a sample image", async () => {
   const image = await main(
     { type: "string", value: "John Smith" },
+    { type: "string", value: undefined },
+    { type: "string", value: process.env.GENERATED_IMAGES_API_KEY }
+  );
+  expect(image).toBeDefined();
+});
+
+test("We can get a sample image with joy emotion", async () => {
+  const image = await main(
+    { type: "string", value: "John Smith" },
+    { type: "string", value: "joy" },
     { type: "string", value: process.env.GENERATED_IMAGES_API_KEY }
   );
   expect(image).toBeDefined();
--- src/column.ts
+++ src/column.ts
@@ -3,19 +3,53 @@ import "whatwg-fetch";
 import { Face, FacesResponse, findSize } from "./faces";
 import * as glide from "./glide";
 
-let page = 0;
+type FaceSet = {
+  currentPage: number;
+  unassigned: Face[];
+  assigned: Record<string, Face>;
+};
 
-const unassigned: Face[] = [];
-const assigned: Record<string, Face> = {};
+type FaceParams = {
+  emotion: string;
+};
 
-async function getMoreFaces(key: string) {
-  page++;
+function makeParams(params: Partial<FaceParams>): FaceParams {
+  return {
+    emotion: params.emotion ?? "neutral",
+  };
+}
+
+function makeParamsKey(params: FaceParams): string {
+  return JSON.stringify(params);
+}
+
+const faceSets: Record<string, FaceSet> = {};
+
+function getFaceSet(partial: Partial<FaceParams>): FaceSet {
+  const key = makeParamsKey(makeParams(partial));
+  const set: FaceSet = faceSets[key] ?? {
+    currentPage: 0,
+    unassigned: [],
+    assigned: {},
+  };
+  return (faceSets[key] = set);
+}
+
+async function getMoreFaces(
+  key: string,
+  partialParams: Partial<FaceParams> = {}
+) {
+  const params = makeParams(partialParams);
+  const faceSet = getFaceSet(params);
+
+  faceSet.currentPage++;
 
   const { faces }: FacesResponse = await fetch(
     `https://api.generated.photos/api/v1/faces` +
       `?order_by=oldest` +
-      `&page=${page}` +
+      `&page=${faceSet.currentPage}` +
       `&per_page=100` +
+      `&emotion=${params.emotion}` +
       `&api_key=${key}`,
     {
       headers: {
@@ -26,21 +60,23 @@ async function getMoreFaces(key: string) {
     }
   ).then((x) => x.json());
 
-  unassigned.push(...faces);
+  faceSet.unassigned.push(...faces);
 }
 
-export default glide.column(async (seed, key) => {
+export default glide.column(async (seed, emotion, key) => {
   if (key.value === undefined) throw new Error("Missing key");
   if (seed.value === undefined) return undefined;
 
   const faceKey = `${seed.value}`;
+  const faceParams = { emotion: emotion.value };
+  const { assigned, unassigned } = getFaceSet(faceParams);
 
   let face = assigned[faceKey];
   while (face === undefined) {
     if (unassigned.length > 0) {
       face = assigned[faceKey] = unassigned.pop()!;
     } else {
-      await getMoreFaces(key.value);
+      await getMoreFaces(key.value, faceParams);
     }
   }
 
