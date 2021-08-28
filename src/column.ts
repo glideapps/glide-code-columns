5f96e6c930ebda9cd65b686bc60ada2e5036e94e
David Siegel <djsiegel@gmail.com>
2021-08-28 14:53:35 -0700

    Small change to test cache busting

--- src/column.ts
+++ src/column.ts
@@ -2,5 +2,5 @@ import * as glide from "./glide";
 
 export default glide.column(async (message) => {
   if (message.value === undefined) return undefined;
-  return `echo ${message.value}`;
+  return `echo ${message.value}!`;
 });
