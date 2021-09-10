2e9ec20b64acda1a8ff68b2a40e6e3579141bf05
David Siegel <djsiegel@gmail.com>
2021-09-09 23:17:55 -0700

    Initial commit

--- src/column.test.ts
+++ src/column.test.ts
@@ -1,6 +1,14 @@
-import main from "./column";
+import { sumNodes } from "./column";
 
-test("We can get an echo", async () => {
-  const response = await main({ type: "string", value: "foo" });
-  expect(response).toBe("echo foo");
+test("We can get a count", async () => {
+  await sumNodes(process.env.REDIS!, "main", "node1", 1, new Date().toString());
+  await sumNodes(process.env.REDIS!, "main", "node2", 1, new Date().toString());
+  const count = await sumNodes(
+    process.env.REDIS!,
+    "main",
+    "node3",
+    1,
+    new Date().toString()
+  );
+  expect(count).toBe(3);
 });
--- src/column.ts
+++ src/column.ts
@@ -1,6 +1,55 @@
 import * as glide from "./glide";
 
-export default glide.column(async (message) => {
-  if (message.value === undefined) return undefined;
-  return `echo ${message.value}`;
+import { createClient } from "redis";
+
+export async function sumNodes(
+  redis: string,
+  counter: string,
+  node: string,
+  count: number,
+  updated: string
+): Promise<number> {
+  const client = createClient({
+    socket: {
+      url: redis,
+    },
+  });
+
+  client.on("error", (err: any) => {
+    throw new Error(`Redis Client Error ${err}`);
+  });
+
+  await client.connect();
+
+  const counterNode = `${counter}:${node}`;
+  const lastUpdate = await client.get(counterNode);
+  if (new Date(lastUpdate).getTime() < new Date(updated).getTime()) {
+    await client.set(counterNode, updated);
+    await client.hSet(counter, { [node]: `${count}` });
+  }
+
+  const counts = await client.hVals(counter);
+  const countSum = counts
+    .map((c) => Number.parseInt(c, 10))
+    .reduce((acc, n) => acc + n, 0);
+
+  await client.disconnect();
+
+  return countSum;
+}
+
+export default glide.column(async (redis, counter, node, count, updated) => {
+  if (redis.value === undefined) return undefined;
+  if (counter.value === undefined) return undefined;
+  if (node.value === undefined) return undefined;
+  if (count.value === undefined) return undefined;
+  if (updated.value === undefined) return undefined;
+
+  return await sumNodes(
+    redis.value,
+    counter.value,
+    node.value,
+    count.value,
+    updated.value
+  );
 });
