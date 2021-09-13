3305152a505d69b759851a77d897af16695407aa
David Siegel <djsiegel@gmail.com>
2021-09-13 08:00:47 -0700

    Fix build

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
+/**
+ * @type {import('next').NextConfig}
+ */
 module.exports = {
   webpack: (config, { isServer }) => {
     // Fixes npm packages that depend on `fs` module
     if (!isServer) {
-      config.node = {
-        fs: "empty",
-      };
+      config.resolve.fallback.fs = false;
     }
     return config;
   },
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,3 +22,4 @@ export default () => {
 
   return <div>fetch column</div>;
 };
+psu;
