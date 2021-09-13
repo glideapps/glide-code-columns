5f3eaea72887e7cc43598432fb5f23da53d1a45b
David Siegel <djsiegel@gmail.com>
2021-09-12 19:30:20 -0700

    This should do it

--- src/pages/api/inc.ts
+++ src/pages/api/inc.ts
@@ -2,7 +2,11 @@ import { allowCors } from "../../glide-next";
 import { connect } from "../../redis";
 
 export default allowCors(async (req, res) => {
-  const { counter } = req.body;
+  const {
+    params: {
+      counter: { value: counter },
+    },
+  } = req.body;
   const client = await connect();
   const count = await client.increment(counter);
   await client.disconnect();
