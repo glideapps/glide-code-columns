5c167bee32a984e0c28b1231841e55412bb8f040
David Siegel <djsiegel@gmail.com>
2021-09-12 19:25:31 -0700

    Also allow query

--- src/pages/api/peek.ts
+++ src/pages/api/peek.ts
@@ -2,7 +2,7 @@ import { allowCors } from "../../glide-next";
 import { connect } from "../../redis";
 
 export default allowCors(async (req, res) => {
-  const { counter } = req.body;
+  const counter = req.body?.counter ?? req.query.counter;
   const client = await connect();
   const count = await client.getBasicCount(counter);
   await client.disconnect();
