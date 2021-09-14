789288b180f1edd6392e3e71e62767d86bf820c7
David Siegel <djsiegel@gmail.com>
2021-09-13 20:28:00 -0700

    Get clever

---
+++ src/glide-react.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import * as glide from "./glide";
+import { useRouter } from "next/dist/client/router";
+
+export * from "./glide";
+
+// TODO type this
+export type Manifest = any;
+
+export interface ColumnProps {
+  calculate: glide.GlideColumn;
+}
+
+// Loads the expected manifest to display and wire the column
+export const Column: React.FC<ColumnProps> = (props) => {
+  const { children, calculate } = props;
+  const router = useRouter();
+  const [manifest, setManifest] = useState<Manifest | undefined>(undefined);
+
+  useEffect(() => {
+    fetch(`${router.asPath}/glide.json`)
+      .then((x) => x.json())
+      .then(setManifest);
+
+    glide.column(calculate);
+  }, []);
+
+  return (
+    <div>
+      <pre>{JSON.stringify(manifest, null, 2)}</pre>
+      {children}
+    </div>
+  );
+};
+
+export function column(props: ColumnProps): React.VFC {
+  return () => <Column {...props} />;
+}
--- src/pages/jq.tsx
+++ src/pages/jq.tsx
@@ -1,24 +1,17 @@
-import { useEffect } from "react";
-import * as glide from "../glide";
-import { ColumnValue } from "../glide";
+import * as glide from "../glide-react";
 import jq from "jq-web";
 
-function tranform(json: ColumnValue, query: ColumnValue) {
-  if (json.value === undefined) {
-    return undefined;
-  }
-  if (query.value === undefined) {
-    return json.value;
-  }
-  const val = jq.json(JSON.parse(json.value), query.value);
-  return typeof val === "object" || Array.isArray(val)
-    ? JSON.stringify(val)
-    : val;
-}
-
-export default () => {
-  useEffect(() => {
-    glide.column(tranform);
-  });
-  return <div>jq column</div>;
-};
+export default glide.column({
+  calculate(json, query) {
+    if (json.value === undefined) {
+      return undefined;
+    }
+    if (query.value === undefined) {
+      return json.value;
+    }
+    const val = jq.json(JSON.parse(json.value), query.value);
+    return typeof val === "object" || Array.isArray(val)
+      ? JSON.stringify(val)
+      : val;
+  },
+});
