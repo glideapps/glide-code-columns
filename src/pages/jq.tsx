42e774f4e8b657fb927514aa1cad2c97e765f949
David Siegel <djsiegel@gmail.com>
2021-09-14 15:29:46 -0700

    Build manifests

--- src/pages/jq.tsx
+++ src/pages/jq.tsx
@@ -1,16 +1,39 @@
-import * as glide from "../glide.next";
-
 import jq from "jq-web";
+import React from "react";
+import { Column } from "../glide.next";
 
-export default glide.column(async (json, query) => {
-  if (json.value === undefined) {
-    return undefined;
-  }
-  if (query.value === undefined) {
-    return json.value;
-  }
-  const val = jq.json(JSON.parse(json.value), query.value);
-  return typeof val === "object" || Array.isArray(val)
-    ? JSON.stringify(val)
-    : val;
-});
+const JQColumn = () => (
+  <Column
+    name="Glide JQ Column"
+    description="Transform JSON with JQ"
+    author="David Siegel <david@glideapps.com>"
+    params={[
+      {
+        name: "json",
+        displayName: "JSON",
+        type: "string",
+      },
+      {
+        name: "query",
+        displayName: "JQ Query",
+        type: "string",
+      },
+    ]}
+    result={{ type: "string" }}
+  >
+    {async (json, query) => {
+      if (json.value === undefined) {
+        return undefined;
+      }
+      if (query.value === undefined) {
+        return json.value;
+      }
+      const val = jq.json(JSON.parse(json.value), query.value);
+      return typeof val === "object" || Array.isArray(val)
+        ? JSON.stringify(val)
+        : val;
+    }}
+  </Column>
+);
+
+export default JQColumn;
