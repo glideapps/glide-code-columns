0fe58e88f8aca3957e00b60ddee07663112c19df
David Siegel <dvdsgl@users.noreply.github.com>
2021-09-19 22:21:29 +0000

    QR code column

---
+++ src/pages/qrcode.tsx
@@ -0,0 +1,43 @@
+import { Column, ColumnComponent } from "../glide.next";
+import { url } from "../urls";
+
+// TODO support more from https://api.qrserver.com/v1/create-qr-code/
+
+const run: Column = async (dataValue, sizeValue) => {
+  const { value: data } = dataValue;
+  const { value: size = 500 } = sizeValue;
+
+  if (data === undefined) {
+    return undefined;
+  }
+
+  return url(`https://api.qrserver.com/v1/create-qr-code/`, {
+    data,
+    size: `${size}x${size}`,
+    margin: 0,
+  });
+};
+
+const QRCodeColumn = () => (
+  <ColumnComponent
+    name="Barcode"
+    description="Generate QR codes with https://api.qrserver.com/v1/create-qr-code/"
+    author="David Siegel <david@glideapps.com>"
+    params={[
+      {
+        name: "content",
+        displayName: "Content",
+        type: "primitive",
+      },
+      {
+        name: "size",
+        displayName: "Size",
+        type: "number",
+      },
+    ]}
+    result={{ type: "image-uri" }}
+    run={run}
+  />
+);
+
+export default QRCodeColumn;
