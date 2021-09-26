959cc2d152523aeb3c2f05e0d505b964db74d6cd
David Siegel <dvdsgl@users.noreply.github.com>
2021-09-26 00:15:14 +0000

    Hell yes code

---
+++ src/pages/hell-yes-code.tsx
@@ -0,0 +1,48 @@
+import { Column, ColumnComponent } from "../glide.next";
+
+const functions = new Map();
+
+const run: Column = async (code, ...params) => {
+  if (code === undefined) return undefined;
+
+  const functionCode = `async (p1,p2,p3)=>{ ${code.value} }`;
+  const fn = eval(functionCode);
+  functions.set(code, fn);
+
+  return fn(...params.map((p) => p.value));
+};
+
+const HellYesCodeColumn = () => (
+  <ColumnComponent
+    name="Hell Yes-Code"
+    description="Runs JavaScript"
+    author="Mark Probst <mark@glideapps.com>"
+    params={{
+      code: {
+        displayName: "JavaScript code",
+        type: "string",
+      },
+      p1: {
+        displayName: "p1",
+        type: "primitive",
+      },
+      p2: {
+        displayName: "p2",
+        type: "primitive",
+      },
+      p3: {
+        displayName: "p3",
+        type: "primitive",
+      },
+    }}
+    example={{
+      code: `return "Hell " + p1.replace("no", "yes") + "!".repeat(p2)`,
+      p1: `no-code`,
+      p2: 5,
+    }}
+    result={{ type: "primitive" }}
+    run={run}
+  />
+);
+
+export default HellYesCodeColumn;
