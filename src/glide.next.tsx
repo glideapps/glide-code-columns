bb52771476e6fde538c902818b66e5107a5becac
David Siegel <dvdsgl@users.noreply.github.com>
2021-09-28 14:33:05 +0000

    style

--- src/glide.next.tsx
+++ src/glide.next.tsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from "react";
+import { SVGProps, useEffect, useState } from "react";
 import * as glide from "./glide";
 import { useRouter } from "next/dist/client/router";
 import { Column } from "./glide";
 
 export * from "./glide";
-import { DuplicateIcon } from "@heroicons/react/solid";
+import { DuplicateIcon } from "@heroicons/react/outline";
 import type { ManifestConvenient } from "./manifest";
 
 type Props<TColumnParams> = ManifestConvenient<TColumnParams> & {
@@ -115,6 +115,18 @@ const REPL: React.VFC<Props<any>> = (props) => {
   );
 };
 
+const GitHubIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    role="img"
+    viewBox="0 0 25 24"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <title>GitHub</title>
+    <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
+  </svg>
+);
+
 // Loads the expected manifest to display and wire the column
 export function ColumnComponent<TColumnParams>(props: Props<TColumnParams>) {
   const { run, ...manifest } = props;
@@ -130,39 +142,33 @@ export function ColumnComponent<TColumnParams>(props: Props<TColumnParams>) {
     return null;
   }
 
-  const installUrl =
-    host === undefined ? undefined : `https://${host}${router.asPath}`;
+  const installUrl = host === undefined ? undefined : `${host}${router.asPath}`;
   const github = `https://github.com/glideapps/glide-code-columns/blob/master/src/pages${router.asPath}.tsx`;
 
   return (
     <div className="h-screen max-w-screen-lg p-5 mx-auto space-y-2 bg-gray-100 dark:bg-black dark:text-white">
-      <div className="md:flex">
-        <div className="flex-grow">
-          <h3 className="text-2xl font-medium">{manifest.name}</h3>
-          <p className="max-w-2xl mt-1 mb-4 opacity-80">
-            {manifest.description}
-          </p>
+      <div className="items-start md:flex">
+        <div className="flex-grow mb-4">
+          <div className="flex items-center space-x-2">
+            <h3 className="text-xl font-medium">{manifest.name}</h3>
+            <GitHubIcon
+              className="w-6 h-6 opacity-50 cursor-pointer hover:opacity-100"
+              onClick={() => window.open(github, "_blank")}
+            />
+          </div>
+          <p className="max-w-2xl text-sm opacity-80">{manifest.description}</p>
         </div>
-        <dl className="">
-          <Row title="URL">
-            {installUrl && (
-              <div className="flex items-center space-x-2">
-                <div className="opacity-70">{installUrl}</div>
-                <DuplicateIcon
-                  className="w-5 h-5 cursor-pointer opacity-80 hover:opacity-100"
-                  onClick={() => {
-                    void navigator.clipboard.writeText(installUrl);
-                  }}
-                />
-              </div>
-            )}
-          </Row>
-          <Row title="Source">
-            <a className="text-blue-500" href={github} target="_blank">
-              Open on GitHub
-            </a>
-          </Row>
-        </dl>
+        {installUrl && (
+          <div
+            className="flex items-center py-1 pl-3 pr-2 space-x-2 font-mono text-xs bg-white border rounded cursor-pointer dark:border-gray-600 dark:bg-gray-800"
+            onClick={() => {
+              void navigator.clipboard.writeText(`https://${installUrl}`);
+            }}
+          >
+            <div className="opacity-70">{installUrl}</div>
+            <DuplicateIcon className="w-5 h-5 opacity-60 hover:opacity-100" />
+          </div>
+        )}
       </div>
       <REPL {...props} />
       <div className="absolute right-4 bottom-4">
