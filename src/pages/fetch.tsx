7f7cd1163785fafe12466c282583e7e5fa1f4802
David Siegel <dvdsgl@users.noreply.github.com>
2021-09-28 22:02:30 +0000

    Simple in-memory cache

--- src/pages/fetch.tsx
+++ src/pages/fetch.tsx
@@ -1,11 +1,28 @@
 import { Column, ColumnComponent } from "../glide.next";
 import jq from "jq-web";
 
+// Simple time-based cache to avoid spamming servers.
+// We tried Cache API before but it's disallowed by our sandboxing.
+const cacheMaxAgeSeconds = 5 * 60;
+const cache = new Map<string, { timestamp: Date; json: any }>();
+
+async function fetchCachedJSON(url: string): Promise<any> {
+  const { timestamp, json } = cache.get(url) ?? { timestamp: new Date(0) };
+  const ageSeconds = (new Date().getTime() - timestamp.getTime()) / 1000;
+  if (ageSeconds < cacheMaxAgeSeconds) {
+    return json;
+  }
+
+  const newJson = await fetch(url).then(x => x.json());
+  cache.set(url, { timestamp: new Date(), json: newJson });
+  return newJson;
+}
+
 const run: Column = async (url, query) => {
   if (url.value === undefined) {
     return undefined;
   }
-  let json = await fetch(url.value).then((x) => x.json());
+  let json = await fetchCachedJSON(url.value);
   if (query.value !== undefined) {
     json = jq.json(json, query.value);
   }
