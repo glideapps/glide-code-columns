5972ef95a3f041e802bc9fb488bfe7c53e59ece8
David Siegel <dvdsgl@users.noreply.github.com>
2021-10-07 15:20:29 -0700

    Optional icons (#8)
    * Optional icons
    
    * Color
    
    * More icons
    
    * More icons
    
    * Cleaner descriptions
    
    * Prepare to embed

--- src/pages/translate.tsx
+++ src/pages/translate.tsx
@@ -1,4 +1,3 @@
-import jq from "jq-web";
 import React from "react";
 import { Column, ColumnComponent } from "../glide.next";
 
@@ -61,6 +60,11 @@ const GoogleTranslateColumn = () => (
       text: "Hello, world!",
       language: "es",
     }}
+    icon={`
+      <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M25.74 30.15L20.66 25.13L20.72 25.07C24.2 21.19 26.68 16.73 28.14 12.01H34V8H20V4H16V8H2V11.98H24.34C22.99 15.84 20.88 19.5 18 22.7C16.14 20.63 14.6 18.38 13.38 16H9.38C10.84 19.26 12.84 22.34 15.34 25.12L5.17 35.17L8 38L18 28L24.22 34.22L25.74 30.15ZM37 20H33L24 44H28L30.25 38H39.75L42 44H46L37 20ZM31.75 34L35 25.33L38.25 34H31.75Z" fill="currentColor"/>
+      </svg>
+    `}
   />
 );
 
--- src/pages/youtube-thumbnail.tsx
+++ src/pages/youtube-thumbnail.tsx
@@ -14,7 +14,7 @@ function thumbnailFromLink(link: string): string | undefined {
     : `https://i.ytimg.com/vi/${id}/maxresdefault.jpg`;
 }
 
-const run: Column = async (link) => {
+const run: Column = async link => {
   if (link.value === undefined) {
     return undefined;
   }
@@ -35,6 +35,11 @@ const YouTubeThumbnailColumn = () => (
     result={{ type: "image-uri" }}
     example={{ videoUrl: "https://www.youtube.com/watch?v=dQw4w9WgXcQ" }}
     run={run}
+    icon={`
+      <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M47.5 14.4C47.5 14.4 47 11.1 45.6 9.59998C43.8 7.69998 41.7 7.69998 40.8 7.59998C34.1 7.09998 24 7.09998 24 7.09998C24 7.09998 13.9 7.09998 7.2 7.59998C6.3 7.69998 4.2 7.69998 2.4 9.59998C1 11.1 0.5 14.4 0.5 14.4C0.5 14.4 0 18.3 0 22.2V25.8C0 29.7 0.5 33.6 0.5 33.6C0.5 33.6 1 36.9 2.4 38.4C4.2 40.3 6.6 40.3 7.7 40.5C11.5 40.9 24 41 24 41C24 41 34.1 41 40.8 40.5C41.7 40.4 43.8 40.4 45.6 38.5C47 37 47.5 33.7 47.5 33.7C47.5 33.7 48 29.8 48 25.9V22.3C48 18.3 47.5 14.4 47.5 14.4ZM19 30.2V16.7L32 23.5L19 30.2Z" fill="#202124"/>
+</svg>
+    `}
   />
 );
 
