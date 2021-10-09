f920d9768fb53e6ac52ae425ad7a37698f0f31b3
David Siegel <dvdsgl@users.noreply.github.com>
2021-10-09 23:16:47 +0000

    Try at top level

--- script/build.sh
+++ script/build.sh
@@ -13,9 +13,11 @@ do
     node public/$SLUG/manifest.js
     rm public/$SLUG/manifest.js
 
-    # Now we build the actual column
-    esbuild $COLUMN --bundle --minify --sourcemap --outdir=public/$SLUG --external:fs --external:path --splitting --format=esm
-    echo "<script type='module'>import './$SLUG/$SLUG.js';</script>" > public/$SLUG/index.html
+
+    echo "<script type='module'>import './$SLUG.js';</script>" > public/$SLUG/index.html
 done
+
+# Now we build the actual column
+esbuild src/columns/*.ts --bundle --minify --sourcemap --outdir=public/ --external:fs --external:path --splitting --format=esm
 
 ts-node script/build.ts
