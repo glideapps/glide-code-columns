cc73f284d4c7f972dd4d4ee54f6587737c11c5a2
David Siegel <dvdsgl@users.noreply.github.com>
2021-10-09 20:28:09 +0000

    WIP

--- script/build.sh
+++ script/build.sh
@@ -8,13 +8,13 @@ do
     mkdir -p public/$SLUG
 
     # First build is to extract metadata and build manifest
-    esbuild $COLUMN --bundle --outfile=public/$SLUG/index.js --format=cjs --log-level=silent --platform=node
+    esbuild $COLUMN --bundle --outfile=public/$SLUG/index.js --format=cjs --log-level=silent --external:fs --external:path
     cp script/manifest.js public/$SLUG
     node public/$SLUG/manifest.js
     rm public/$SLUG/manifest.js
 
     # Now we build the actual column
-    esbuild $COLUMN --bundle --outfile=public/$SLUG/index.js --minify --sourcemap --platform=node # node could be wrong
+    esbuild $COLUMN --bundle --outfile=public/$SLUG/index.js --minify --sourcemap --external:fs --external:path
     echo "<script src='/$SLUG/index.js'></script>" > public/$SLUG/index.html
 done
 
