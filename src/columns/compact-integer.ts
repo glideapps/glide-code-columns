d56d581006efe327ae99ac8a3069ab0ed976cc28
David Siegel <dvdsgl@users.noreply.github.com>
2021-10-20 13:59:53 +0000

    Humanize Compact Integer

---
+++ src/columns/compact-integer.ts
@@ -0,0 +1,13 @@
+import * as glide from "../glide";
+
+import Humanize from "humanize-plus";
+
+export default glide
+  .columnNamed("Compact Integer")
+  .withDescription(`Converts an integer into its most compact representation.`)
+  .withAuthor("HubSpot", "https://github.com/HubSpot/humanize")
+  .withStringResult()
+  .withRequiredNumberParam("number")
+  .withNumberParam("decimals", "Decimal Places")
+  .withExample({ number: 123456789 })
+  .run(({ number, decimals }) => Humanize.compactInteger(number, decimals));
