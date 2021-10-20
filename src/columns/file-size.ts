1e5ba150e8815d97c6a7832d5a2f35fc738f87b8
David Siegel <dvdsgl@users.noreply.github.com>
2021-10-20 14:03:27 +0000

    File size

---
+++ src/columns/file-size.ts
@@ -0,0 +1,14 @@
+import * as glide from "../glide";
+
+import Humanize from "humanize-plus";
+
+export default glide
+  .columnNamed("File Size")
+  .withDescription(
+    `Formats the value like a 'human-readable' file size (i.e. '13 KB', '4.1 MB', '102 bytes', etc).`
+  )
+  .withAuthor("HubSpot", "https://github.com/HubSpot/humanize")
+  .withStringResult()
+  .withRequiredNumberParam("fileSize")
+  .withExample({ fileSize: 1024 * 2000 })
+  .run(({ fileSize }) => Humanize.fileSize(fileSize));
