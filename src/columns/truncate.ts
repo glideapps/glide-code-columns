06539bcfb9ebcb49fc93842f340f083715895059
David Siegel <dvdsgl@users.noreply.github.com>
2021-10-20 14:09:06 +0000

    Truncate

---
+++ src/columns/truncate.ts
@@ -0,0 +1,18 @@
+import * as glide from "../glide";
+
+import Humanize from "humanize-plus";
+
+export default glide
+  .columnNamed("Truncate Text")
+  .withDescription(
+    `Truncates a string if it is longer than the specified number of characters. Truncated strings will end with a translatable ellipsis sequence ("…").`
+  )
+  .withAuthor("HubSpot", "https://github.com/HubSpot/humanize")
+  .withStringResult()
+  .withRequiredStringParam("string")
+  .withNumberParam("length")
+  .withStringParam("ending")
+  .withExample({ string: "long text is good for you", length: 19 })
+  .run(({ string, length, ending }) =>
+    Humanize.truncate(string, length, ending)
+  );
