375dbdd7da13d00ef9194e6122f2a81bf4de2efb
David Siegel <dvdsgl@users.noreply.github.com>
2021-10-20 14:06:13 +0000

    Pluralize

---
+++ src/columns/pluralize.ts
@@ -0,0 +1,18 @@
+import * as glide from "../glide";
+
+import Humanize from "humanize-plus";
+
+export default glide
+  .columnNamed("Pluralize")
+  .withDescription(
+    `Returns the plural version of a given word if the value is not 1. The default suffix is 's'.`
+  )
+  .withAuthor("HubSpot", "https://github.com/HubSpot/humanize")
+  .withStringResult()
+  .withRequiredStringParam("number")
+  .withNumberParam("singular")
+  .withNumberParam("plural")
+  .withExample({ number: 3, singular: "duck", plural: "duckies" })
+  .run(({ number, singular, plural }) =>
+    Humanize.pluralize(number, singular, plural)
+  );
