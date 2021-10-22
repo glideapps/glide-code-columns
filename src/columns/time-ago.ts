8b851b8700a7436fe0f33a7429eb2346bac7a7f1
David Siegel <dvdsgl@users.noreply.github.com>
2021-10-22 04:59:23 +0000

    Add styles

--- src/columns/time-ago.ts
+++ src/columns/time-ago.ts
@@ -1,7 +1,9 @@
 import * as glide from "../glide";
 
-import TimeAgo from "javascript-time-ago";
+import TimeAgo, { FormatStyleName } from "javascript-time-ago";
 import en from "javascript-time-ago/locale/en.json";
+
+const styles: FormatStyleName[] = ["round", "mini", "twitter"];
 
 TimeAgo.addDefaultLocale(en);
 const timeAgo = new TimeAgo("en-US");
@@ -21,11 +23,13 @@ export default glide
   .withStringResult()
   .withRequiredDateParam("date")
   .withDateParam("now")
+  .withStringParam("style", `Style (${styles.join(", ")})`)
 
   .withTest(
     {
       now: "2021-10-21T14:35:46.216Z",
       date: "2020-10-21T14:35:46.216Z",
+      style: "round",
     },
     "1 year ago"
   )
@@ -33,6 +37,7 @@ export default glide
     {
       now: "2021-10-21T14:35:46.216Z",
       date: "2021-10-21T14:35:46.216Z",
+      style: "round",
     },
     "just now"
   )
@@ -40,16 +45,17 @@ export default glide
     {
       now: "2021-10-21T14:35:46.216Z",
       date: "2021-10-21T14:40:46.216Z",
+      style: "round",
     },
     "in 5 minutes"
   )
 
-  .run(({ date, now }) => {
+  .run(({ date, now, style }) => {
     const dateMillis = Date.parse(date);
     if (isNaN(dateMillis)) return undefined;
 
     const nowMillis = now === undefined ? Date.now() : Date.parse(now);
     if (isNaN(nowMillis)) return undefined;
 
-    return timeAgo.format(dateMillis, "round", { now: nowMillis }) as string;
+    return timeAgo.format(dateMillis, style, { now: nowMillis }) as string;
   });
