bf68208467418d848a7687cc51cc79c8bd3966d9
David Siegel <dvdsgl@users.noreply.github.com>
2021-10-22 16:46:01 -0700

    Add URL encoding (#27)
    * Add URL encoding
    
    * Descriptions

--- src/columns/decode.ts
+++ src/columns/decode.ts
@@ -1,10 +1,25 @@
 import * as glide from "../glide";
+import { encodings, encodingTypes } from "./encode";
 
 export default glide
-  .columnNamed("Base64 Decode")
+  .columnNamed("Decode Text")
   .withCategory("Encoding")
-  .withDescription("A common binary encoding for text.")
-  .withStringResult()
+  .withDescription("Decode text in a variety of formats.")
+
   .withRequiredStringParam("text")
-  .withTest({ text: `SGVsbG8sIHdvcmxkIQ==` }, "Hello, world!")
-  .run(({ text }) => atob(text));
+  .withStringParam("encoding", `Encoding (${encodingTypes.join(", ")})`)
+  .withStringResult()
+
+  .withTest({ text: "SGVsbG8sIHdvcmxkIQ==" }, `Hello, world!`)
+  .withTest(
+    { text: "SGVsbG8sIHdvcmxkIQ==", encoding: "base64" },
+    `Hello, world!`
+  )
+  .withTest({ text: "Hello%2C%20world!", encoding: "url" }, `Hello, world!`)
+
+  .run(({ text, encoding = "base64" }) => {
+    if (!encodingTypes.includes(encoding)) return undefined;
+
+    const { decode } = encodings[encoding];
+    return decode?.(text);
+  });
--- src/columns/encode.ts
+++ src/columns/encode.ts
@@ -1,13 +1,45 @@
 import * as glide from "../glide";
+import { withWindow } from "../util";
+
+type Encoder = (x: string) => string;
+
+type Encoding = {
+  encode: Encoder | undefined;
+  decode: Encoder | undefined;
+};
+
+export const encodings: Record<string, Encoding> = {
+  base64: {
+    encode: withWindow(w => w.btoa),
+    decode: withWindow(w => w.atob),
+  },
+  url: {
+    encode: encodeURIComponent,
+    decode: decodeURIComponent,
+  },
+};
+
+export const encodingTypes = Object.keys(encodings);
 
 export default glide
-  .columnNamed("Base64 Encode")
+  .columnNamed("Encode Text")
   .withCategory("Encoding")
-  .withDescription("A common binary encoding for text.")
+  .withDescription("Encode text in a variety of formats.")
 
   .withRequiredStringParam("text")
+  .withStringParam("encoding", `Encoding (${encodingTypes.join(", ")})`)
   .withStringResult()
 
   .withTest({ text: `Hello, world!` }, "SGVsbG8sIHdvcmxkIQ==")
+  .withTest(
+    { text: `Hello, world!`, encoding: "base64" },
+    "SGVsbG8sIHdvcmxkIQ=="
+  )
+  .withTest({ text: `Hello, world!`, encoding: "url" }, "Hello%2C%20world!")
 
-  .run(({ text }) => btoa(text));
+  .run(({ text, encoding = "base64" }) => {
+    if (!encodingTypes.includes(encoding)) return undefined;
+
+    const { encode } = encodings[encoding];
+    return encode?.(text);
+  });
