805636ba22ca6a98e3f4d69ac2286ece425d0d64
David Siegel <dvdsgl@users.noreply.github.com>
2021-10-25 07:28:48 -0700

    Bayes Classifier (#30)
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * FIX

---
+++ src/columns/classify.ts
@@ -0,0 +1,41 @@
+import * as glide from "../glide";
+
+interface Classifier {
+  learn(sample: string, category: string);
+  categorize(doc: string): string;
+  toJson(): string;
+}
+
+const bayes = require("bayes") as {
+  (): Classifier;
+  fromJson(json: string): Classifier;
+};
+
+const defaultClassifier = "";
+
+const classifiers: Record<string, Classifier> = {};
+function getClassifier(id: string): Classifier {
+  return (classifiers[id] ??= bayes());
+}
+
+export default glide
+  .columnNamed("Classify Text")
+  .withCategory("Machine Learning")
+  .withDescription(`Categorize text based on examples.`)
+  .withAuthor("ttezel", "https://github.com/ttezel/bayes")
+
+  .withRequiredStringParam("phrase")
+  .withStringParam("category", "Training Data")
+  .withStringParam("id", "Classifier ID")
+  .withStringResult()
+
+  .run(({ id = defaultClassifier, phrase, category }) => {
+    const classifer = getClassifier(id);
+
+    if (category !== undefined) {
+      classifer.learn(phrase, category);
+      return category;
+    } else {
+      return classifer.categorize(phrase);
+    }
+  });
