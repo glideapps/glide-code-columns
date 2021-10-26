7d7e6ae214cc9627a41301acee1fc779813ce392
Seth Rosetter <seth.rosetter@gmail.com>
2021-10-26 19:43:07 +0000

    Add seed

--- src/columns/lorem-ipsum.ts
+++ src/columns/lorem-ipsum.ts
@@ -1,25 +1,26 @@
 import * as glide from "../glide";
+
 import { LoremIpsum } from "lorem-ipsum";
+import seedrandom from "seedrandom";
 
 export default glide
-  .columnNamed("Lorem Ipsum")
-  .withCategory("Text")
-  .withDescription(`Produces a 'lorem ipsum' style string`)
-  .withAuthor("davidbau", "github.com/davidbau/seedrandom")
-  .withAuthor("knicklabs", "github.com/knicklabs/lorem-ipsum.js")
-  .withStringResult()
-  .withRequiredNumberParam(
-    "numberOfWordsPerSentence",
-    "Number of words per sentence"
-  )
-  .withRequiredNumberParam("numberOfSentences", "Number of sentences")
-  .run(({ numberOfWordsPerSentence, numberOfSentences }) => {
-    const lorem = new LoremIpsum({
-      wordsPerSentence: {
-        min: numberOfWordsPerSentence,
-        max: numberOfWordsPerSentence,
-      },
+    .columnNamed("Lorem Ipsum")
+    .withCategory("Text")
+    .withDescription(`Produces a 'lorem ipsum' style string`)
+    .withAuthor("davidbau", "github.com/davidbau/seedrandom")
+    .withAuthor("knicklabs", "github.com/knicklabs/lorem-ipsum.js")
+    .withStringResult()
+    .withRequiredNumberParam("numberOfWordsPerSentence", "Number of words per sentence")
+    .withRequiredNumberParam("numberOfSentences", "Number of sentences")
+    .withTest({ numberOfWordsPerSentence: 2, numberOfSentences: 3 }, "Aliqua duis. Incididunt culpa. Commodo enim.")
+    .run(({ numberOfWordsPerSentence, numberOfSentences }) => {
+        const lorem = new LoremIpsum({
+            wordsPerSentence: {
+                min: numberOfWordsPerSentence,
+                max: numberOfWordsPerSentence,
+            },
+            random: seedrandom("Glide")
+        });
+        const text = lorem.generateSentences(numberOfSentences);
+        return text;
     });
-    const text = lorem.generateSentences(numberOfSentences);
-    return text;
-  });
