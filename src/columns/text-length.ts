4886893cfb69513d661c74c6e83e3e7b3c5feb8e
Daniel Sweet <danieljsweet@icloud.com>
2021-10-26 12:27:52 -0700

    Low level text manipulation

---
+++ src/columns/text-length.ts
@@ -0,0 +1,11 @@
+import * as glide from "../glide";
+
+export default glide
+  .columnNamed("Text Length")
+  .withCategory("Text")
+  .withDescription(`Gets the length of a text snippet`)
+  .withNumberResult()
+  .withRequiredStringParam("text")
+  // FIXME: Length of naughty strings
+  .withTest({ text: "This is some text" }, 17)
+  .run(({ text }) => text.length);
