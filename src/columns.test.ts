bc28837b573665a89350931fc57d0562485827b5
David Siegel <dvdsgl@users.noreply.github.com>
2021-10-26 16:18:54 -0700

    Allow failing tests so we can provide examples (#53)
    * Allow failing tests so we can provide examples
    
    * Allow failures

--- src/columns.test.ts
+++ src/columns.test.ts
@@ -8,21 +8,30 @@ Object.values(definitions).forEach(definition => {
 
     if (tests.length === 0) return;
 
-    for (const { params: testParams, expectedResult } of tests) {
+    for (const { params: testParams, expectedResult, allowFailure } of tests) {
         const paramsDescription = Object.entries(testParams)
             .map(([k, v]) => `${k}: ${JSON.stringify(v)}`)
             .join(", ");
-        const description = `${name} { ${paramsDescription} } = ${JSON.stringify(expectedResult)}`;
+        const description = `${name} { ${paramsDescription} } = ${JSON.stringify(expectedResult)}${
+            allowFailure === true ? " FAILURE EXPECTED" : ""
+        }`;
         test(description, async () => {
-            const params = Object.entries(staticParams).map(
-                ([name, columnParam]) =>
-                    ({
-                        type: columnParam.type,
-                        value: testParams[name],
-                    } as ColumnValue)
-            );
-            const result = await definition.run(...params);
-            expect(result).toStrictEqual(expectedResult);
+            try {
+                const params = Object.entries(staticParams).map(
+                    ([name, columnParam]) =>
+                        ({
+                            type: columnParam.type,
+                            value: testParams[name],
+                        } as ColumnValue)
+                );
+
+                const result = await definition.run(...params);
+                expect(result).toStrictEqual(expectedResult);
+            } catch (error) {
+                if (allowFailure !== true) {
+                    throw error;
+                }
+            }
         });
     }
 });
