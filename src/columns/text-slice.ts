35f0cc994eb9a83182005c677797dc01a3a69fc8
Daniel Sweet <djsweet@users.noreply.github.com>
2021-10-26 13:21:38 -0700

    Add low-level text manipulation (#40)
    * Low level text manipulation
    
    * Nth argument is now zero based

---
+++ src/columns/text-slice.ts
@@ -0,0 +1,15 @@
+import * as glide from "../glide";
+
+export default glide
+  .columnNamed("Text Slice")
+  .withCategory("Text")
+  .withDescription(`Gets a substring out of text, from zero-indexed "start" with an optional length`)
+  .withStringResult()
+  .withRequiredStringParam("text")
+  .withRequiredNumberParam("start")
+  .withNumberParam("length")
+  .withTest({ text: "substring this", start: 0 }, "substring this")
+  .withTest({ text: "substring this", start: 3 }, "string this")
+  .withTest({ text: "substring this", start: 3, length: 6 }, "string")
+  .withTest({ text: "substring this", start: 0, length: 3 }, "sub")
+  .run(({ text, start, length }) => text.substring(start, length === undefined ? undefined : length + start));
