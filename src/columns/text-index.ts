2fea12442d139bdd5798dcadfc099f54fdb29432
Daniel Sweet <danieljsweet@icloud.com>
2021-10-26 12:32:55 -0700

    Nth argument is now zero based

--- src/columns/text-index.ts
+++ src/columns/text-index.ts
@@ -3,15 +3,15 @@ import * as glide from "../glide";
 export default glide
   .columnNamed("Text Offset")
   .withCategory("Text")
-  .withDescription(`Gets the zero-based offset of the (one-based) N-th occurence of needle in haystack`)
+  .withDescription(`Gets the zero-based offset of the zero-based N-th occurence of needle in haystack`)
   .withNumberResult()
   .withRequiredStringParam("haystack")
   .withRequiredStringParam("needle")
   .withNumberParam("nth")
   // Simple 1 entry test
   .withTest({ haystack: "there is one @ here", needle: "@" }, 13)
-  .withTest({ haystack: "there is one @ here", needle: "@", nth: 0 }, undefined)
-  .withTest({ haystack: "there is one @ here", needle: "@", nth: 1 }, 13)
+  .withTest({ haystack: "there is one @ here", needle: "@", nth: 0 }, 13)
+  .withTest({ haystack: "there is one @ here", needle: "@", nth: 1 }, undefined)
   .withTest({ haystack: "there is one @ here", needle: "@", nth: 2 }, undefined)
   .withTest({ haystack: "there is one @ here", needle: "@", nth: -1 }, 13)
   .withTest({ haystack: "there is one @ here", needle: "@", nth: -2 }, undefined)
@@ -26,33 +26,32 @@ export default glide
   .withTest({ haystack: "there are two @ here in @ and there", needle: "@", nth: -3 }, undefined)
   .withTest({ haystack: "there are two @ here in @ and there", needle: "@", nth: -2 }, 14)
   .withTest({ haystack: "there are two @ here in @ and there", needle: "@", nth: -1 }, 24)
-  .withTest({ haystack: "there are two @ here in @ and there", needle: "@", nth: 0 }, undefined)
-  .withTest({ haystack: "there are two @ here in @ and there", needle: "@", nth: 1 }, 14)
-  .withTest({ haystack: "there are two @ here in @ and there", needle: "@", nth: 2 }, 24)
+  .withTest({ haystack: "there are two @ here in @ and there", needle: "@", nth: 0 }, 14)
+  .withTest({ haystack: "there are two @ here in @ and there", needle: "@", nth: 1 }, 24)
+  .withTest({ haystack: "there are two @ here in @ and there", needle: "@", nth: 2 }, undefined)
   .withTest({ haystack: "there are two @ here in @ and there", needle: "@", nth: 3 }, undefined)
   // Two entry in the middle
   .withTest({ haystack: "@ but there's one in the front @ and middle", needle: "@", nth: -3 }, undefined)
   .withTest({ haystack: "@ but there's one in the front @ and middle", needle: "@", nth: -2 }, 0)
   .withTest({ haystack: "@ but there's one in the front @ and middle", needle: "@", nth: -1 }, 31)
-  .withTest({ haystack: "@ but there's one in the front @ and middle", needle: "@", nth: 0 }, undefined)
-  .withTest({ haystack: "@ but there's one in the front @ and middle", needle: "@", nth: 1 }, 0)
-  .withTest({ haystack: "@ but there's one in the front @ and middle", needle: "@", nth: 2 }, 31)
+  .withTest({ haystack: "@ but there's one in the front @ and middle", needle: "@", nth: 0 }, 0)
+  .withTest({ haystack: "@ but there's one in the front @ and middle", needle: "@", nth: 1 }, 31)
+  .withTest({ haystack: "@ but there's one in the front @ and middle", needle: "@", nth: 2 }, undefined)
   .withTest({ haystack: "@ but there's one in the front @ and middle", needle: "@", nth: 3 }, undefined)
   // Three, front and back
   .withTest({ haystack: "@ and we want @ the second @", needle: "@" }, 0)
-  .withTest({ haystack: "@ and we want @ the second @", needle: "@", nth: 2 }, 14)
-  .withTest({ haystack: "@ and we want @ the second @", needle: "@", nth: 3 }, 27)
+  .withTest({ haystack: "@ and we want @ the second @", needle: "@", nth: 1 }, 14)
+  .withTest({ haystack: "@ and we want @ the second @", needle: "@", nth: 2 }, 27)
   .withTest({ haystack: "@ and we want @ the second @", needle: "@", nth: -1 }, 27)
   .withTest({ haystack: "@ and we want @ the second @", needle: "@", nth: -2 }, 14)
   .withTest({ haystack: "@ and we want @ the second @", needle: "@", nth: -3 }, 0)
   .withTest({ haystack: "@ and we want @ the second @", needle: "@", nth: -4 }, undefined)
   // Longer numbers
   .withTest({ haystack: "and == we can do == long == numbers ==", needle: "==" }, 4)
-  .withTest({ haystack: "and == we can do == long == numbers ==", needle: "==", nth: 2 }, 17)
-  .withTest({ haystack: "and == we can do == long == numbers ==", needle: "==", nth: 3 }, 25)
+  .withTest({ haystack: "and == we can do == long == numbers ==", needle: "==", nth: 1 }, 17)
+  .withTest({ haystack: "and == we can do == long == numbers ==", needle: "==", nth: 2 }, 25)
   .run(({ needle, haystack, nth }) => {
-    let runs = nth ?? 1, foundIndex = -1;
-    if (runs === 0) return undefined;
+    let runs = Math.floor(nth ?? 0), foundIndex = -1;
 
     if (runs < 0) {
       runs = -runs;
@@ -63,7 +62,9 @@ export default glide
         if (foundIndex < 0) break;
       }
     } else {
+      runs++;
       let accumIndex = 0;
+
       for (let i = 0; i < runs; i++) {
         haystack = haystack.substring(foundIndex + 1);
         foundIndex = haystack.indexOf(needle);
