1d801761deccb403186603daa7b7614431f4d4be
Bry Nguyen <brynguyen1331@gmail.com>
2021-10-26 21:11:05 +0000

    Handle array instead of single value

--- src/columns/prefix-string.ts
+++ src/columns/prefix-string.ts
@@ -1,22 +1,30 @@
 import * as glide from "../glide";
 
 export default glide
-  .columnNamed("Prefix Text")
-  .withCategory("Text")
-  .withDescription(
-    `Returns the result text from a given prefix and text value that are joined together.`
-  )
-  .withRequiredStringParam("text")
-  .withRequiredStringParam("prefix")
-  .withStringResult()
+    .columnNamed("Prefix Array of Text")
+    .withCategory("Text")
+    .withDescription(`Returns an array of prefixed text from a given prefix and text value that are joined together.`)
+    .withStringArrayResult()
+    .withRequiredStringArrayParam("text")
+    .withRequiredStringParam("prefix")
 
-  .withTest({ text: "$3.99", prefix: "Price per cat:" }, "Price per cat: $3.99")
-  .withTest(
-    { text: "14 minutes", prefix: "Summer Sale starts in:" },
-    "Summer Sale starts in: 14 minutes"
-  )
-  .withTest({ text: "25", prefix: "Average MPG:" }, "Average MPG: 25")
+    .withTest({ text: ["$3.99", "$6.00", "$450.00"], prefix: "Meal cost per cat:" }, [
+        "Meal cost per cat: $3.99",
+        "Meal cost per cat: $6.00",
+        "Meal cost per cat: $450.00",
+    ])
+    .withTest({ text: ["14 minutes", "12:03:07"], prefix: "Summer Sale starts in:" }, [
+        "Summer Sale starts in: 14 minutes",
+        "Summer Sale starts in: 12:03:07",
+    ])
+    .withTest({ text: ["25", "12.4", "45"], prefix: "Average MPG:" }, [
+        "Average MPG: 25",
+        "Average MPG: 12.4",
+        "Average MPG: 45",
+    ])
 
-  .run(({ text, prefix }) => {
-    return `${prefix} ${text}`;
-  });
+    .run(({ text, prefix }) =>
+        text.map(el => {
+            return `${prefix} ${el}`;
+        })
+    );
