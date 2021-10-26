97824f802b653cff012b8f981fdeb7947c2092b5
Jeremy Llewellyn <jeremy.llewellyn@gmail.com>
2021-10-26 13:22:31 -0700

    Add author

--- src/columns/parts-of-speech.ts
+++ src/columns/parts-of-speech.ts
@@ -5,26 +5,24 @@ const model = require("wink-eng-lite-web-model");
 const nlp = winkNLP(model);
 
 export default glide
-  .columnNamed("Parts of Speech Extractor")
-  .withCategory("Text")
-  .withReleased("direct")
-  .withDescription(`Parts of Speech extractor`)
-  .withStringResult()
-  .withRequiredStringParam("sentence")
-  .withRequiredStringParam(
-    "pos",
-    "https://winkjs.org/wink-nlp/part-of-speech.html"
-  )
+    .columnNamed("Parts of Speech Extractor")
+    .withCategory("Text")
+    .withAuthor("WinkJS", "https://github.com/winkjs/wink-nlp")
+    .withReleased("direct")
+    .withDescription(`Parts of Speech extractor`)
+    .withStringResult()
+    .withRequiredStringParam("sentence")
+    .withRequiredStringParam("pos", "https://winkjs.org/wink-nlp/part-of-speech.html")
 
-  .withTest({ sentence: "Sally kicked the ball", pos: "VERB" }, "kicked")
-  .withTest({ sentence: "Sally kicked the ball", pos: "PROPN" }, "Sally")
-  .withTest({ sentence: "Sally kicked the ball", pos: "NOUN" }, "ball")
+    .withTest({ sentence: "Sally kicked the ball", pos: "VERB" }, "kicked")
+    .withTest({ sentence: "Sally kicked the ball", pos: "PROPN" }, "Sally")
+    .withTest({ sentence: "Sally kicked the ball", pos: "NOUN" }, "ball")
 
-  .run(({ sentence, pos }) => {
-    return nlp
-      .readDoc(sentence)
-      .tokens()
-      .filter(token => token.out(nlp.its.pos) === pos)
-      .out()
-      .join(" ");
-  });
+    .run(({ sentence, pos }) => {
+        return nlp
+            .readDoc(sentence)
+            .tokens()
+            .filter(token => token.out(nlp.its.pos) === pos)
+            .out()
+            .join(" ");
+    });
