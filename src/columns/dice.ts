57416f9c7a564e01fb506f9846018eb2f2aadf80
David Siegel <djsiegel@gmail.com>
2021-10-26 07:47:01 -0700

    Fix negative-sided die

--- src/columns/dice.ts
+++ src/columns/dice.ts
@@ -2,9 +2,11 @@ import * as glide from "../glide";
 
 import seedrandom from "seedrandom";
 
+function roll(seed: any, sides: 0): undefined;
+function roll(seed: any, sides: number): number;
 function roll(seed: any, sides: number): number | undefined {
   if (sides === 0) return undefined;
-  if (sides < 0) return roll(seed, -sides);
+  if (sides < 0) return -roll(seed, -sides);
 
   const random = seedrandom(seed)();
   return Math.floor(sides * random) + 1;
@@ -21,6 +23,7 @@ export default glide
 
   .withTest({ seed: "I'm feeling lucky!" }, 1)
   .withTest({ seed: "I'm feeling lucky!", sides: 6 }, 1)
+  .withTest({ seed: "I'm feeling lucky!", sides: -6 }, -1)
   .withTest({ seed: "I'm feeling lucky!", sides: 0 }, undefined)
 
   .run(({ seed, sides = 6 }) => roll(seed, sides));
