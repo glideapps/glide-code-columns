6769f0e42dc0a73dfe78b744fc178d7dd1b0f27a
David Siegel <djsiegel@gmail.com>
2021-10-29 06:42:36 -0700

    Icons per category

--- src/glide.ts
+++ src/glide.ts
@@ -1,4 +1,5 @@
 import startCase from "lodash/startCase";
+import { defaultIcon, getIconForManifest } from "./icons";
 
 export type PrimitiveColumnType = "string" | "primitive" | "number" | "boolean" | "image-uri" | "date-time" | "uri";
 
@@ -95,7 +96,7 @@ export type Manifest = {
 
 export type ManifestConvenient<T> = Omit<Manifest, "params" | "icon"> & {
     params: { [Name in keyof T]: Omit<ColumnParam, "name"> };
-    icon?: "glide" | string;
+    icon?: string;
 };
 
 type Test<TParams> = { params: Partial<TParams>; expectedResult: any; allowFailure?: boolean };
@@ -120,40 +121,11 @@ export function column<TColumnParams>(manifest: ColumnDefinition<TColumnParams>)
     };
 }
 
-const glideIcon = `
-        <svg
-          width="48"
-          height="48"
-          viewBox="0 0 26 27"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M0 14.85L14.3 0V7.29C14.3 11.4653 11.0406 14.85 7.02 14.85H0Z"
-            fill="currentColor"
-          />
-          <path
-            d="M11.7 19.71C11.7 15.5347 14.9594 12.15 18.98 12.15H26L11.7 27V19.71Z"
-            fill="currentColor"
-          />
-        </svg>
-    `;
-
-const defaultIcon = `<svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M12.0002 34.828L2.58624 25.414C1.80524 24.633 1.80524 23.367 2.58624 22.586L12.0002 13.172L14.8292 16L6.82924 24L14.8292 32L12.0002 34.828Z" fill="currentColor"/>
-<path d="M36.0004 34.828L33.1714 32L41.1714 24L33.1714 16L36.0004 13.172L45.4144 22.586C46.1954 23.367 46.1954 24.633 45.4144 25.414L36.0004 34.828Z" fill="currentColor"/>
-<path d="M26.5485 5.57617L17.5723 41.4553L21.4527 42.4261L30.4289 6.54697L26.5485 5.57617Z" fill="currentColor"/>
-</svg>`;
-
 export function toStrictManifest(convenient: ManifestConvenient<any>): Manifest {
     // We carefully pick out just the props in manifest, because more
     // could come in from the component.
     const { name, category, released, description, author, result, params, about, video } = convenient;
-
-    let { icon = defaultIcon } = convenient;
-    if (icon === "glide") {
-        icon = glideIcon;
-    }
+    const icon = getIconForManifest(convenient);
 
     return {
         name,
