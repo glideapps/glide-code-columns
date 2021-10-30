cf0f51782791b777b6cce4141485fa81c2d37802
David Siegel <dvdsgl@users.noreply.github.com>
2021-10-29 17:11:44 -0700

    Not column (#59)
    * Not column
    
    * Not required
    
    * Use Boolean

---
+++ src/columns/not.ts
@@ -0,0 +1,16 @@
+import * as glide from "../glide";
+
+export default glide
+    .columnNamed("Not")
+    .withCategory("General")
+    .withReleased("direct")
+    .withDescription(`True becomes false. False (and blank) become true.`)
+
+    .withBooleanParam("value")
+    .withBooleanResult()
+
+    .withTest({ value: true }, false)
+    .withTest({ value: false }, true)
+    .withTest({ value: undefined }, true)
+
+    .run(({ value }) => !Boolean(value));
