6378ab53cc14475507801fd429360c7a6a714f4e
David Siegel <dvdsgl@users.noreply.github.com>
2021-10-30 13:01:41 -0700

    Is Truthy & Is Falsey (#60)
    * Truthiness column
    
    * Falsey
    
    * move
    
    * Less confusing
    
    * WIP
    
    * trim
    
    * WIP
    
    * Handle stringified zero

---
+++ src/columns/falsey.ts
@@ -0,0 +1,40 @@
+import * as glide from "../glide";
+
+export function isFalsey(x: any): boolean {
+    if (typeof x === "string") {
+        const n = parseFloat(x);
+        if (!isNaN(n)) {
+            return n === 0;
+        }
+        return ["", "no", "false"].includes(x.trim().toLowerCase());
+    }
+    if (Array.isArray(x)) {
+        return x.length === 0;
+    }
+    return Boolean(x) === false;
+}
+
+export default glide
+    .columnNamed("Is Falsey")
+    .withCategory("General")
+    .withReleased("direct")
+    .withDescription(`True if value is false or false-like, otherwise false.`)
+
+    .withPrimitiveParam("value")
+    .withBooleanResult()
+
+    .withTest({ value: true }, false)
+    .withTest({ value: "true" }, false)
+    .withTest({ value: "TRUE" }, false)
+    .withTest({ value: "yes" }, false)
+    .withTest({ value: "" }, true)
+    .withTest({ value: [] }, true)
+    .withTest({ value: 0 }, true)
+    .withTest({ value: "0" }, true)
+    .withTest({ value: "false" }, true)
+    .withTest({ value: "FALSE" }, true)
+    .withTest({ value: "no" }, true)
+    .withTest({ value: undefined }, true)
+    .withTest({ value: null }, true)
+
+    .run(({ value }) => isFalsey(value));
