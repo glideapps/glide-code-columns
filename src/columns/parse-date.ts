d1701ffe483c97d710182dc4e332f8571edd813b
David Siegel <djsiegel@gmail.com>
2021-10-30 13:18:33 -0700

    Parse date

---
+++ src/columns/parse-date.ts
@@ -0,0 +1,37 @@
+import * as glide from "../glide";
+
+import { DateTime } from "luxon";
+
+export default glide
+    .columnNamed("Text to Date")
+    .withDescription(`Convert text to a date.`)
+    .withReleased("direct")
+    .withCategory("Date & Time")
+    .withAuthor("luxon", "https://moment.github.io/luxon")
+    .withAbout(
+        `
+    Learn about date formatting in [luxon's documentation](https://moment.github.io/luxon/#/formatting?id=table-of-tokens).
+  `
+    )
+
+    .withRequiredStringParam("text")
+    .withStringParam("format", "Format (see https://gl.ink/date-format)")
+    .withStringParam("locale")
+    .withStringParam("zone", "Timezone")
+    .withDateResult()
+
+    .withTest({ text: "12 August 1982", format: "d MMMM yyyy" }, new Date("1982-08-12T07:00:00.000Z"))
+
+    .run(({ text, format, locale, zone }) => {
+        let dt: DateTime;
+        const options = { locale, zone };
+        if (format === undefined) {
+            dt = DateTime.fromISO(text, options);
+        } else {
+            dt = DateTime.fromFormat(text, format, options);
+        }
+        if (!dt.isValid) {
+            return undefined;
+        }
+        return dt.toJSDate();
+    });
