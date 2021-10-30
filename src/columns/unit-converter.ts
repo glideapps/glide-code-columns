7d217c0f6bf39711dcdd26b1d172ba75421edc4b
David Siegel <djsiegel@gmail.com>
2021-10-30 12:51:04 -0700

    Unit converter

---
+++ src/columns/unit-converter.ts
@@ -0,0 +1,25 @@
+import * as glide from "../glide";
+
+import * as math from "mathjs";
+
+export default glide
+    .columnNamed("Convert Units")
+    .withDescription(`Convert a quantity from one unit to another.`)
+
+    .withCategory("Number")
+    .withReleased("direct")
+    .withNumberResult()
+    .withRequiredNumberParam("number")
+    .withRequiredStringParam("from", "From Unit")
+    .withRequiredStringParam("to", "To Unit")
+
+    // Fails because of floating point
+    .withFailingTest({ number: 1, from: "ft", to: "in" }, 12)
+
+    .run(({ number, from, to }) => {
+        try {
+            return math.unit(number, from).toNumber(to);
+        } catch {
+            return undefined;
+        }
+    });
