e0a821024ab05dd346aa4319b2a02a9434bc6049
David Siegel <djsiegel@gmail.com>
2021-10-30 10:26:26 -0700

    Truthiness column

---
+++ src/columns/truthy.ts
@@ -0,0 +1,35 @@
+import * as glide from "../glide";
+
+function isFalsey(x: any): boolean {
+    if (typeof x === "string") {
+        return ["", "no", "false"].includes(x.toLowerCase());
+    }
+    if (Array.isArray(x)) {
+        return x.length === 0;
+    }
+    return Boolean(x) === false;
+}
+
+export default glide
+    .columnNamed("Is Truthy")
+    .withCategory("General")
+    .withReleased("direct")
+    .withDescription(`True becomes false. False (and blank) become true.`)
+
+    .withPrimitiveParam("value")
+    .withBooleanResult()
+
+    .withTest({ value: true }, true)
+    .withTest({ value: "true" }, true)
+    .withTest({ value: "TRUE" }, true)
+    .withTest({ value: "yes" }, true)
+    .withTest({ value: "" }, false)
+    .withTest({ value: [] }, false)
+    .withTest({ value: 0 }, false)
+    .withTest({ value: "false" }, false)
+    .withTest({ value: "FALSE" }, false)
+    .withTest({ value: "no" }, false)
+    .withTest({ value: undefined }, false)
+    .withTest({ value: null }, false)
+
+    .run(({ value }) => !isFalsey(value));
