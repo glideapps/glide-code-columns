e2d63344714bd02bb8025c106ec7ce85aeaa45dc
David Siegel <dvdsgl@users.noreply.github.com>
2021-10-30 20:34:28 -0700

    Only test changed (#70)

--- src/columns.test.ts
+++ src/columns.test.ts
@@ -1,37 +1,45 @@
-import { getColumnDefinitions } from "./columns";
+import { getColumnDefinition, getColumnSlugs } from "./columns";
 import { ColumnValue } from "./glide";
 
-const definitions = getColumnDefinitions();
+import { execSync } from "child_process";
 
-Object.values(definitions).forEach(definition => {
-    const { name, tests = [], params: staticParams } = definition;
+const changedSlugs = Array.from(
+    execSync("git diff --name-only HEAD", { encoding: "utf-8" }).matchAll(/src\/columns\/(.+)\.ts/g)
+).map(x => x[1]);
+
+const slugsToTest = changedSlugs.length > 0 ? changedSlugs : getColumnSlugs();
+
+slugsToTest.forEach(slug => {
+    const { run, tests = [], params: staticParams } = getColumnDefinition(slug);
 
     if (tests.length === 0) return;
 
-    for (const { params: testParams, expectedResult, allowFailure } of tests) {
-        const paramsDescription = Object.entries(testParams)
-            .map(([k, v]) => `${k}: ${JSON.stringify(v)}`)
-            .join(", ");
-        const description = `${name} { ${paramsDescription} } = ${JSON.stringify(expectedResult)}${
-            allowFailure === true ? " FAILURE EXPECTED" : ""
-        }`;
-        test(description, async () => {
-            try {
-                const params = Object.entries(staticParams).map(
-                    ([name, columnParam]) =>
-                        ({
-                            type: columnParam.type,
-                            value: testParams[name],
-                        } as ColumnValue)
-                );
+    describe(slug, () => {
+        for (const { params: testParams, expectedResult, allowFailure } of tests) {
+            const paramsDescription = Object.entries(testParams)
+                .map(([k, v]) => `${k}: ${JSON.stringify(v)}`)
+                .join(", ");
+            const description = `{ ${paramsDescription} } = ${JSON.stringify(expectedResult)}${
+                allowFailure === true ? " FAILURE EXPECTED" : ""
+            }`;
+            test(description, async () => {
+                try {
+                    const params = Object.entries(staticParams).map(
+                        ([name, columnParam]) =>
+                            ({
+                                type: columnParam.type,
+                                value: testParams[name],
+                            } as ColumnValue)
+                    );
 
-                const result = await definition.run(...params);
-                expect(result).toStrictEqual(expectedResult);
-            } catch (error) {
-                if (allowFailure !== true) {
-                    throw error;
+                    const result = await run(...params);
+                    expect(result).toStrictEqual(expectedResult);
+                } catch (error) {
+                    if (allowFailure !== true) {
+                        throw error;
+                    }
                 }
-            }
-        });
-    }
+            });
+        }
+    });
 });
