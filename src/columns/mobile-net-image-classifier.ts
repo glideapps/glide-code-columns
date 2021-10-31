2d6d4969162468f3464bd6b4b5b3c55e6016d309
Ian Leatherbury <ian.leatherbury@gmail.com>
2021-10-31 20:43:44 +0000

    initial commit

---
+++ src/columns/mobile-net-image-classifier.ts
@@ -0,0 +1,16 @@
+import * as glide from "../glide";
+
+/* Example from https://github.com/tensorflow/tfjs-models/tree/master/mobilenet
+const mobilenet = require('@tensorflow-models/mobilenet');
+
+const img = document.getElementById('img');
+
+// Load the model.
+const model = await mobilenet.load();
+
+// Classify the image.
+const predictions = await model.classify(img);
+
+console.log('Predictions: ');
+console.log(predictions);
+*/
