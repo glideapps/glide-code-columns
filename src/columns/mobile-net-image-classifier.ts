0a071057676bd62826210f34bb7da72d9bbb881c
Ian Leatherbury <ian.leatherbury@gmail.com>
2021-10-31 23:45:13 +0000

    Basic stub

--- src/columns/mobile-net-image-classifier.ts
+++ src/columns/mobile-net-image-classifier.ts
@@ -14,3 +14,15 @@ const predictions = await model.classify(img);
 console.log('Predictions: ');
 console.log(predictions);
 */
+
+const run: glide.Column = async (image) => {
+    return "test";
+};
+
+export default glide
+    .columnNamed("Classify Images")
+    .withCategory("Machine Learning")
+    .withReleased("direct")
+    .withDescription("Returns an array of most likely predictions and their confidences")
+    .withTest("Test", "test")
+    .run(run)
