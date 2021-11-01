ac1f7a7c854b7b8cdbc35fc760d92ab38c5d58fa
Ian Leatherbury <ian.leatherbury@gmail.com>
2021-11-01 02:08:25 +0000

    basic working version

--- src/columns/mobile-net-image-classifier.ts
+++ src/columns/mobile-net-image-classifier.ts
@@ -1,28 +1,43 @@
 import * as glide from "../glide";
+import "@tensorflow/tfjs-backend-cpu";
+import "@tensorflow/tfjs-backend-webgl";
 
-/* Example from https://github.com/tensorflow/tfjs-models/tree/master/mobilenet
-const mobilenet = require('@tensorflow-models/mobilenet');
+// Example from https://github.com/tensorflow/tfjs-models/tree/master/mobilenet
+const version = 2;
+const alpha = 0.5;
 
-const img = document.getElementById('img');
+const mobilenet = require("@tensorflow-models/mobilenet");
 
-// Load the model.
-const model = await mobilenet.load();
+async function loadImage(uri) {
+    console.log(uri);
+    return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.crossOrigin = "anonymous";
+        img.src = uri.text;
+        img.onload = () => resolve(img);
+        img.onerror = reject;
+    });
+}
 
-// Classify the image.
-const predictions = await model.classify(img);
+const run: glide.Column = async imageUri => {
+    // Load the model.
+    const model = await mobilenet.load();
 
-console.log('Predictions: ');
-console.log(predictions);
-*/
+    var img = await loadImage(imageUri);
 
-const run: glide.Column = async (image) => {
-    return "test";
+    // Classify the image.
+    const predictions = await model.classify(img);
+
+    const labels = predictions.map(p => [p.className, p.probability.toFixed(2)]);
+    labels.sort((l1, l2) => l2[1] - l1[1]);
+    return labels.map(p => p.join(":"));
 };
 
 export default glide
     .columnNamed("Classify Images")
     .withCategory("Machine Learning")
+    .withRequiredStringParam("text")
     .withReleased("direct")
     .withDescription("Returns an array of most likely predictions and their confidences")
-    .withTest("Test", "test")
-    .run(run)
+    .withFailingTest("https://images.unsplash.com/photo-1495360010541-f48722b34f7d", "test")
+    .run(run);
