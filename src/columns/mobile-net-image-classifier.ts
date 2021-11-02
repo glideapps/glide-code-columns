df877e38b0e975808e571f4649edb0577773369e
Ian Leatherbury <ian.leatherbury@gmail.com>
2021-11-02 02:05:38 +0000

    update version and clean a little

--- src/columns/mobile-net-image-classifier.ts
+++ src/columns/mobile-net-image-classifier.ts
@@ -3,13 +3,12 @@ import "@tensorflow/tfjs-backend-cpu";
 import "@tensorflow/tfjs-backend-webgl";
 
 // Example from https://github.com/tensorflow/tfjs-models/tree/master/mobilenet
+const mobilenet = require("@tensorflow-models/mobilenet");
 const version = 2;
 const alpha = 0.5;
 
-const mobilenet = require("@tensorflow-models/mobilenet");
-
 async function loadImage(uri) {
-    console.log(uri);
+    // From https://replit.com/@clementwalter/mobilenet
     return new Promise((resolve, reject) => {
         const img = new Image();
         img.crossOrigin = "anonymous";
@@ -21,15 +20,19 @@ async function loadImage(uri) {
 
 const run: glide.Column = async imageUri => {
     // Load the model.
-    const model = await mobilenet.load();
+    const model = await mobilenet.load({version, alpha});
 
     var img = await loadImage(imageUri);
 
     // Classify the image.
     const predictions = await model.classify(img);
 
-    const labels = predictions.map(p => [p.className, p.probability.toFixed(2)]);
+    const labels = predictions.map(p => [p.className, p.probability.toFixed(2)]);    
+
     labels.sort((l1, l2) => l2[1] - l1[1]);
+
+    console.log(labels)
+
     return labels.map(p => p.join(":"));
 };
 
@@ -39,5 +42,5 @@ export default glide
     .withRequiredStringParam("text")
     .withReleased("direct")
     .withDescription("Returns an array of most likely predictions and their confidences")
-    .withFailingTest("https://images.unsplash.com/photo-1495360010541-f48722b34f7d", "test")
+    .withFailingTest("https://images.unsplash.com/photo-1495360010541-f48722b34f7d", "https://images.unsplash.com/photo-1495360010541-f48722b34f7d")
     .run(run);
