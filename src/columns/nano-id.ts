9c47035d5f0ff56a32fa4a99142f0f3fc93223bc
Ian Leatherbury <ian.leatherbury@gmail.com>
2021-11-03 17:20:23 +0000

    Add custom alphabet support and cleanup

--- src/columns/nano-id.ts
+++ src/columns/nano-id.ts
@@ -1,19 +1,24 @@
 import * as glide from "../glide";
-import { nanoid } from "../../node_modules/nanoid/async";
+import { nanoid, customAlphabet } from "../../node_modules/nanoid/async";
 
 export default glide
     .columnNamed("Nano Id")
     .withCategory("Text")
     .withDescription(`A tiny (108 bytes), secure, URL-friendly, unique string ID generator for JavaScript`)
-    .withNumberParam("num", "Number")
+    .withNumberParam("num", "Length")
+    .withStringParam("alphabet", "Custom Alphabet")
     .withAuthor("Andrey Sitnik", "https://github.com/ai/nanoid")
-    .withFailingTest(6, "test")
+    .withFailingTest({ num: 10, alphabet: "" }, "test")
     .withAbout(
         `
         A tiny, secure, URL-friendly, unique string ID generator for JavaScript.`
     )
     .withStringResult()
-    .run(async ({ num }) => {
-        const id = await nanoid(num);
-        return id;
+    .run(async ({ alphabet, num }) => {
+        if (alphabet) {
+            const nanoid = customAlphabet(alphabet, num);
+            return await nanoid();
+        } else {
+            return await nanoid(num);
+        }        
     });
