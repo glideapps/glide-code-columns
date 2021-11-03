59eaf5b9ccac46179db634967a2a8e9ff0953c7d
Ian Leatherbury <ian.leatherbury@gmail.com>
2021-11-03 15:52:41 +0000

    Add optional length param

--- src/columns/nano-id.ts
+++ src/columns/nano-id.ts
@@ -1,19 +1,19 @@
 import * as glide from "../glide";
 import { nanoid } from "../../node_modules/nanoid/async";
 
-const run: glide.Column = async () => {
-    const id = await nanoid();
-    return id;
-};
-
 export default glide
     .columnNamed("Nano Id")
     .withCategory("Text")
     .withDescription(`A tiny (108 bytes), secure, URL-friendly, unique string ID generator for JavaScript`)
+    .withNumberParam("num", "Number")
     .withAuthor("Andrey Sitnik", "https://github.com/ai/nanoid")
+    .withFailingTest(6, "test")
     .withAbout(
         `
         A tiny, secure, URL-friendly, unique string ID generator for JavaScript.`
     )
     .withStringResult()
-    .run(run);
+    .run(async ({ num }) => {
+        const id = await nanoid(num);
+        return id;
+    });
