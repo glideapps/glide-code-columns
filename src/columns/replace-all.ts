8fbf2891df8845e3f7a94f5a47195cb23c559a3c
David Siegel <dvdsgl@users.noreply.github.com>
2021-11-04 16:35:35 -0700

    Replace all text column (#77)
    * Replace all text column
    
    * meta
    
    * Actually test that it does all.

---
+++ src/columns/replace-all.ts
@@ -0,0 +1,17 @@
+import * as glide from "../glide";
+
+export default glide
+    .columnNamed("Replace All")
+    .withCategory("Text")
+    .withReleased("direct")
+    .withDescription(`Replace every occurrence of some text with some other text.`)
+
+    .withRequiredStringParam("text")
+    .withRequiredStringParam("x", "Replace This")
+    .withRequiredStringParam("y", "With This")
+    .withStringResult()
+
+    .withTest({ text: `Hello, world.`, x: "world", y: "universe" }, "Hello, universe.")
+    .withTest({ text: `Hello, world world.`, x: "world", y: "universe" }, "Hello, universe universe.")
+
+    .run(({ text, x, y }) => text.replace(new RegExp(x, "g"), y));
