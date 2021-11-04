f552239f0859b8fe36fd84906984d2e16629d60d
David Siegel <dvdsgl@users.noreply.github.com>
2021-11-04 23:26:16 +0000

    Replace all text column

---
+++ src/columns/replace-all.ts
@@ -0,0 +1,16 @@
+import * as glide from "../glide";
+
+export default glide
+    .columnNamed("Replace All")
+    .withCategory("Text")
+    .withReleased("direct")
+    .withDescription(`Converts string to upper case.`)
+
+    .withRequiredStringParam("text")
+    .withRequiredStringParam("x", "Replace This")
+    .withRequiredStringParam("y", "With This")
+    .withStringResult()
+
+    .withTest({ text: `Hello, world.`, x: "world", y: "universe" }, "Hello, universe.")
+
+    .run(({ text, x, y }) => text.replace(new RegExp(x, "g"), y));
