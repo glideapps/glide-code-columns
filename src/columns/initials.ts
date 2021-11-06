669e2ac4f7e19af36da00e36bd2cca909def9cb4
David Siegel <dvdsgl@users.noreply.github.com>
2021-11-06 04:24:07 +0000

    Whitespace

--- src/columns/initials.ts
+++ src/columns/initials.ts
@@ -11,19 +11,27 @@ export default glide
     .withBooleanParam("ignoreLowercase")
     .withStringResult()
 
+    .withTest({ text: `Hello World`, separator: "." }, "H.W.")
+
     .withTest({ text: `Hello World` }, "HW")
     .withTest({ text: `Hello` }, "H")
-    .withTest({ text: `Hello World`, separator: "." }, "H.W.")
+    .withTest({ text: `Hello`, separator: "." }, "H")
+
+    .withTest({ text: `   Hello   World  ` }, "HW")
 
     .withTest({ text: `Hello the World`, separator: "." }, "H.t.W.")
     .withTest({ text: `Hello the World`, separator: ".", ignoreLowercase: true }, "H.W.")
     .withTest({ text: `Hello the World`, separator: ".", ignoreLowercase: false }, "H.t.W.")
 
     .run(({ text, separator = "", ignoreLowercase = false }) => {
-        const words = text
+        const initials = text
+            .trim()
             .split(" ")
+            .map(t => t.trim())
+            .filter(t => t !== "")
             .map(x => x.slice(0, 1))
             .filter(i => !ignoreLowercase || i === i.toLocaleUpperCase());
-        const ending = words.length > 1 ? separator : "";
-        return words.join(separator) + ending;
+
+        const ending = initials.length > 1 ? separator : "";
+        return initials.join(separator) + ending;
     });
