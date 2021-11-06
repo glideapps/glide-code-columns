6f5a88a55e5c02f31f9d3a33d068e5deb505b125
David Siegel <dvdsgl@users.noreply.github.com>
2021-11-06 03:39:17 +0000

    Fallbacks

--- src/columns/gravatar.ts
+++ src/columns/gravatar.ts
@@ -1,5 +1,8 @@
 import * as glide from "../glide";
+
 const gravatar = require("gravatar.js");
+
+const defaults = ["404", "mp", "indenticon", "monsterid", "wavatar", "retro", "robohash", "blank"];
 
 export default glide
     .columnNamed("Photo for Email")
@@ -9,8 +12,17 @@ export default glide
 
     .withRequiredStringParam("email")
     .withNumberParam("size")
+    .withStringParam("defaultIcon", `Fallback ${defaults.join(", ")}`)
     .withImageResult()
 
     .withFailingTest({ email: `david@glideapps.com` }, "???")
 
-    .run(({ email, size = 200 }) => gravatar.resolve(email, { defaultIcon: "404", size }));
+    .run(async ({ email, size = 200, defaultIcon = "404" }) => {
+        try {
+            const response = await gravatar.resolve(email, { defaultIcon, size });
+            console.log({ response });
+            return response;
+        } catch {
+            return undefined;
+        }
+    });
