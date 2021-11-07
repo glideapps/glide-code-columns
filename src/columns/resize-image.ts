9ae1ea7e9b324df69ca768207e6829269f22be79
David Siegel <dvdsgl@users.noreply.github.com>
2021-11-07 23:14:50 +0000

    WIP

--- src/columns/resize-image.ts
+++ src/columns/resize-image.ts
@@ -10,10 +10,17 @@ export default glide
     .withRequiredImageParam("image")
     .withImageResult()
 
+    .withFailingTest(
+        {
+            image: "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/krp9F8PlcbhxIIkDbO5P/pub/QXLhzysH7Zv0BIbUbvbd.png",
+        },
+        ""
+    )
+
     .run(({ image }) => {
         const svg = `
             <svg width="400" height="400" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                <image width="400" height="400" y="" xlink:href="${image}" />
+                <image width="400" height="400"  xlink:href="${image}" />
             </svg>
             `;
         return svgToMiniDataURI(svg);
