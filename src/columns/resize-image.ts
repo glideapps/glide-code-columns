a6d57126639d7663cf9709eb585679f03d90e3ff
David Siegel <dvdsgl@users.noreply.github.com>
2021-11-10 02:05:28 +0000

    Allow replacing with nothing

---
+++ src/columns/resize-image.ts
@@ -0,0 +1,28 @@
+import svgToMiniDataURI from "mini-svg-data-uri";
+import * as glide from "../glide";
+
+export default glide
+    .columnNamed("Resize Image")
+    .withCategory("Image")
+    .withReleased("direct")
+    .withDescription("Beautiful hand-crafted SVG icons, by the makers of Tailwind CSS.")
+
+    .withRequiredImageParam("image")
+    .withImageResult()
+
+    .withFailingTest(
+        {
+            image: "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/krp9F8PlcbhxIIkDbO5P/pub/QXLhzysH7Zv0BIbUbvbd.png",
+        },
+        ""
+    )
+
+    .run(({ image }) => {
+        const svg = `
+            <svg width="400" height="400" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                <image width="400" height="400" xlink:href="${image}" />
+            </svg>
+            `;
+        return svgToMiniDataURI(svg);
+    });
+t;
