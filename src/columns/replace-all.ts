ee2cdd31a7c9ef890211cebec166930912594ec6
David Siegel <dvdsgl@users.noreply.github.com>
2021-11-09 18:08:59 -0800

    Replace-all-allow-empty (#87)
    * Allow replacing with nothing
    
    * Allow replacing with nothing
    
    * not part of this PR

--- src/columns/replace-all.ts
+++ src/columns/replace-all.ts
@@ -8,10 +8,14 @@ export default glide
 
     .withRequiredStringParam("text")
     .withRequiredStringParam("x", "Replace This")
-    .withRequiredStringParam("y", "With This")
+    .withStringParam("y", "With This")
     .withStringResult()
 
     .withTest({ text: `Hello, world.`, x: "world", y: "universe" }, "Hello, universe.")
     .withTest({ text: `Hello, world world.`, x: "world", y: "universe" }, "Hello, universe universe.")
 
-    .run(({ text, x, y }) => text.replace(new RegExp(x, "g"), y));
+    // Allow replacing with nothing
+    .withTest({ text: "ab", x: "a", y: "" }, "b")
+    .withTest({ text: "ab", x: "a" }, "b")
+
+    .run(({ text, x, y = "" }) => text.replace(new RegExp(x, "g"), y));
