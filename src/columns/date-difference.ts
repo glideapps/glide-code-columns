773e010c5f01f9e1c5b952d6fb2bb618a55dfb2f
David Siegel <djsiegel@gmail.com>
2022-06-08 07:16:05 -0700

    working

--- src/columns/date-difference.ts
+++ src/columns/date-difference.ts
@@ -2,19 +2,19 @@ import * as glide from "../glide";
 
 import { DATEDIF } from "fast-formula-parser/formulas/functions/date";
 
-const units = { days: "d" };
+const units = { days: "d", months: "m", years: "y", md: "md", yd: "yd", ym: "ym" };
 
 export default glide
     .columnNamed("Date Difference")
     .withCategory("Date & Time")
     .withReleased("direct")
-    .withDescription("Get the number of days, weeks, months, or years between dates.")
+    .withDescription("Get the number of days, months, or years between dates.")
 
     .withRequiredDateParam("startDate")
     .withRequiredDateParam("endDate")
     .withStringParam("unit", `Unit (${Object.keys(units).join(", ")})`)
     .withNumberResult()
 
-    // .withTest({ text: "SGVsbG8sIHdvcmxkIQ==" }, `Hello, world!`)
+    .withTest({ startDate: "6/1/2022", endDate: "6/2/2022", unit: "days" }, 1)
 
-    .run(({ startDate, endDate, unit = "days" }) => DATEDIF(startDate, endDate, unit));
+    .run(({ startDate, endDate, unit = "days" }) => DATEDIF(startDate, endDate, units[unit] ?? unit));
