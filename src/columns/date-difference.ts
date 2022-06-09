5f6b7bc9d9f6f18f03157a5dc9f84f7755495c17
David Siegel <djsiegel@gmail.com>
2022-06-08 20:54:38 -0700

    wip

--- src/columns/date-difference.ts
+++ src/columns/date-difference.ts
@@ -1,25 +1,28 @@
 import * as glide from "../glide";
 
-import { DateTime, DurationObjectUnits, DurationUnit } from "luxon";
+import { ConversionAccuracy, DateTime, DurationObjectUnits, DurationUnit } from "luxon";
 const units: Array<keyof DurationObjectUnits> = ["days", "weeks", "months", "years"];
 
 export default glide
     .columnNamed("Date Difference")
     .withCategory("Date & Time")
     .withReleased("direct")
-    .withDescription("Get the number of days, months, or years between dates.")
+    .withDescription("Get the number of days, weeks, months, or years between dates.")
 
     .withRequiredDateParam("startDate")
     .withRequiredDateParam("endDate")
-    .withStringParam("unit", `Unit (${units.join(", ")})`)
+    .withRequiredStringParam("unit", `Unit (${units.join(", ")})`)
+    .withStringParam("conversionAccuracy", `Accuracy (casual, longterm)`)
     .withNumberResult()
 
     .withTest({ startDate: "6/1/2022", endDate: "6/2/2022", unit: "days" }, 1)
     .withTest({ startDate: "6/2/2022", endDate: "6/1/2022", unit: "days" }, -1)
 
-    .run(({ startDate, endDate, unit = "days" }) => {
+    .run(({ startDate, endDate, unit = "days", conversionAccuracy = "casual" }) => {
         const start = DateTime.fromJSDate(new Date(startDate));
         const end = DateTime.fromJSDate(new Date(endDate));
-        const diff = end.diff(start, unit as DurationUnit);
+        const diff = end.diff(start, unit as DurationUnit, {
+            conversionAccuracy: conversionAccuracy as ConversionAccuracy,
+        });
         return diff.days;
     });
