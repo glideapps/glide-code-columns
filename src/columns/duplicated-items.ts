4fcbbf7a8d4ddb1527d1bcfecaa8adfee4426044
Daniel Sweet <danieljsweet@icloud.com>
2022-10-21 18:05:57 -0400

    Optimize the duplicated-items column to be ~O(n) instead of O(n^2)

--- src/columns/duplicated-items.ts
+++ src/columns/duplicated-items.ts
@@ -1,7 +1,5 @@
 import * as glide from "../glide";
 
-import uniq from "lodash/uniq";
-
 export default glide
     .columnNamed("Duplicated Items")
     .withCategory("Array")
@@ -15,4 +13,30 @@ export default glide
     .withTest({ values: ["a", "b", "c"] }, [])
     .withTest({ values: undefined }, undefined)
 
-    .run(({ values }) => uniq(values.filter((v: string, ind: number) => values.slice(ind + 1).includes(v))));
+    .run(({ values }) => {
+        const counts = new Map<string, number>();
+        const results: string[] = [];
+        // The previous behavior was:
+        // uniq(values.filter((v, i) => values.slice(i+1).includes(v)))
+        // The filter/slice/includes is an O(n^2) operation, and the uniq
+        // is internally implemented as an O(n^2) operation, though it
+        // happens as a separate step so this is really O(n^2).
+        // For large arrays this is still way too slow, and especially
+        // disastrous on self-joined tables where the array happens to
+        // contain all of the rows.
+        for (let i = values.length - 1; i >= 0; i--) {
+            const value = values[i];
+            const prevCount = counts.get(value);
+            if (prevCount === undefined) {
+                counts.set(value, 1);
+            } else if (prevCount === 1) {
+                counts.set(value, 2);
+                results.unshift(value);
+            } else {
+                // We already know there's a duplicate, we don't need
+                // to increment the count here.
+                continue;
+            }
+        }
+        return results;
+    });
