5e3e272f3955519df66e71408980d2351927e1ae
Daniel Sweet <danieljsweet@icloud.com>
2022-10-22 09:05:14 -0400

    Preserve prior output order

--- src/columns/duplicated-items.ts
+++ src/columns/duplicated-items.ts
@@ -10,11 +10,13 @@ export default glide
 
     .withTest({ values: ["a", "b", "b", "c"] }, ["b"])
     .withTest({ values: ["a", "b", "a", "b", "c", "b"] }, ["a", "b"])
+    .withTest({ values: ["a", "b", "a", "b", "c", "b", "a"] }, ["a", "b"])
     .withTest({ values: ["a", "b", "c"] }, [])
     .withTest({ values: undefined }, undefined)
 
     .run(({ values }) => {
         const counts = new Map<string, number>();
+        const indexes = new Map<string, number>();
         const results: string[] = [];
         // The previous behavior was:
         // uniq(values.filter((v, i) => values.slice(i+1).includes(v)))
@@ -24,19 +26,20 @@ export default glide
         // For large arrays this is still way too slow, and especially
         // disastrous on self-joined tables where the array happens to
         // contain all of the rows.
-        for (let i = values.length - 1; i >= 0; i--) {
+        for (let i = values.length; i >= 0; i--) {
             const value = values[i];
             const prevCount = counts.get(value);
             if (prevCount === undefined) {
                 counts.set(value, 1);
             } else if (prevCount === 1) {
                 counts.set(value, 2);
-                results.unshift(value);
+                indexes.set(value, i);
+                results.push(value);
             } else {
-                // We already know there's a duplicate, we don't need
-                // to increment the count here.
-                continue;
+                // We already know there's a duplicate, we just need
+                // to update the index map so that the lowest index is stored.
+                indexes.set(value, i);
             }
         }
-        return results;
+        return results.sort((l, r) => (indexes.get(l) ?? 0) - (indexes.get(r) ?? 0));
     });
