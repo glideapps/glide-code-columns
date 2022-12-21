fdc7b5e2f9a5564a6d3a7c6dcfa8aaf7d1e20816
David Siegel <dvdsgl@users.noreply.github.com>
2022-12-21 15:43:15 -0600

    Format duration column (#129)
    * Format duration column
    
    * Tests

---
+++ src/columns/format-duration.ts
@@ -0,0 +1,27 @@
+import * as glide from "../glide";
+
+import { Duration } from "luxon";
+
+export default glide
+    .columnNamed("Format Duration")
+    .withDescription(`Format a duration in seconds into text like '1:30'.`)
+    .withReleased("direct")
+    .withCategory("Date & Time")
+    .withAuthor("luxon", "https://moment.github.io/luxon")
+    .withAbout(
+        `
+    Learn about date formatting in [luxon's documentation](https://moment.github.io/luxon/#/formatting?id=table-of-tokens).
+  `
+    )
+    .withStringResult()
+    .withRequiredNumberParam("seconds")
+    .withRequiredStringParam("format", "Format (e.g. mm:ss)")
+
+    .withTest({ seconds: 100, format: "mm:ss" }, "01:40")
+    .withTest({ seconds: 1, format: "ss" }, "01")
+    .withTest({ seconds: 1000000000, format: "yy:MM:dd:hh:mm:ss" }, "31:08:19:01:46:40")
+
+    .run(({ seconds, format }) => {
+        const duration = Duration.fromMillis(seconds * 1000);
+        return duration.toFormat(format);
+    });
